15527bab67e730cb6f70a156658db49cea7ca3bf
Nelson <nlorax@gmail.com>
2021-09-06 21:10:51 -0400

    Add hamburger-icon and Change color palette

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 'use-strict';
 
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const primaryNav = document.querySelector('.primary-nav');
 const animatedBox = document.querySelector('.animatedBox');
+
+const toggleMenu = function () {
+  hamburgerIcon.classList.toggle('clicked');
+  primaryNav.classList.toggle('open');
+};
+hamburgerIcon.onclick = toggleMenu;
+
 const animate = function () {
   animatedBox.classList.toggle('run');
 };
@@ -9,3 +18,4 @@ animatedBox.addEventListener('click', function (e) {
   e.preventDefault();
   animate();
 });
+
