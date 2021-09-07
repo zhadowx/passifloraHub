8c083a306a4ae63e87e421d7366c9f6ca72cbdce
Nelson <nlorax@gmail.com>
2021-09-07 19:52:39 -0400

    Add nav style and sticky menu

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const primaryNav = document.querySelector('.primary-nav');
 const animatedBox = document.querySelector('.animatedBox');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
 
 const toggleMenu = function () {
   hamburgerIcon.classList.toggle('clicked');
@@ -19,3 +21,17 @@ animatedBox.addEventListener('click', function (e) {
   animate();
 });
 
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
